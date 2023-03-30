e9115bd9c0c00b16652c45bee154cfe9ccfdd22a
Ralamboniaina <ralamboniainafano@gmail.com>
2023-03-30 21:46:16 +0300

    Add BaingaWagesCalculator & DailyTesk classes

---
+++ bainga-wages-calculator/bainga-wages-calculator-domain/src/main/java/mg/panomba/bainga_wages_calculator/domain/model/BaingaWagesCalculator.java
@@ -0,0 +1,23 @@
+package mg.panomba.bainga_wages_calculator.domain.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaingaWagesCalculator {
+    public int numberOfPomodoro() {
+        return 0;
+    }
+
+    public int hourSpent() {
+        return 0;
+    }
+
+    public BigDecimal wages() {
+        return BigDecimal.ZERO;
+    }
+
+    public List<DailyTask> dailyTasks() {
+        return new ArrayList<>();
+    }
+}
---
+++ bainga-wages-calculator/bainga-wages-calculator-domain/src/test/java/mg/panomba/bainga_wages_calculator/domain/model/WhenBaingaWagesCalculatorHasBeenInitializedTest.java
@@ -0,0 +1,28 @@
+package mg.panomba.bainga_wages_calculator.domain.model;
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("When bainga wages calculator has been inialized")
+class WhenBaingaWagesCalculatorHasBeenInitializedTest {
+
+    @Test
+    @DisplayName("then report should be empty")
+    void thenReportShouldBeEmpty() {
+        //ARRANGE
+        BaingaWagesCalculator baingaWagesCalculator;
+        //ACT
+        baingaWagesCalculator = new BaingaWagesCalculator();
+        //ASSERT
+        assertThat(baingaWagesCalculator.numberOfPomodoro()).isZero();
+        assertThat(baingaWagesCalculator.hourSpent()).isZero();
+        assertThat(baingaWagesCalculator.wages()).isCloseTo(BigDecimal.ZERO, Percentage.withPercentage(0));
+        assertThat(baingaWagesCalculator.dailyTasks()).isEmpty();
+    }
+
+}
